8ddcf54069cbe2ba97fc688683d936704bb0208f
Adrian Iwaszkiewicz <omojpepe@gmail.com>
2013-10-04 10:29:47 +0100

    Create offlinemessage.php

---
+++ offlinemessage.php
@@ -0,0 +1,16 @@
+<div class="box">
+<center>
+<div class="-dropdown -error-">
+<div class="-dropdown-header">Offline</div>
+<div class="-dropdown-content">
+
+
+<!-- If you came to edit the message, make sure you edit only the text itself. You need to leave <p> and </p> tags so the message displays properly. Thank you ;) -->
+
+
+<?php
+
+echo '<p> Server seems offline. Probably maintenance work is carried out. Should not take long. We apologize for any inconvenience caused.</p>'
+
+?>
+</div>
