a3e691360a35bfa95b8a1b57744ff8f2796728e6
Adrian Iwaszkiewicz <omojpepe@gmail.com>
2013-11-25 16:02:50 +0000

    Create config.php
    Complete redesign of config.php

---
+++ config.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Config file for WebStatus by Adriani6
+This is the only file you should edit. 
+If you do decide to go and edit other files, make sure you know what you're doing.
+
+Make sure you set;
+enable-query=true
+in your server.properties file or it won't work !!
+
+Edit the values below
+Once done, save and check the widget, magic will happen.
+*/
+
+//Server IP
+$ip = "localhost";
+
+//Server Port
+$port = "25565"; 
+
+//Minotar ? Cravatar ?
+$headsurl = "Minotar";
+
+//Head size (in pixels!)
+$headsize = "20";
+
+//Your offline message ! (Currently Now Working !, if your server is offline, it won't show anything !!)
+$offline = "Oh no, we're offline!";
+
+
+/* 
+License: Creative Commons.
+www: http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+(c)Adriani6
+
+Thanks to: 
+xPaw for Minecraft Query Script
+Minotar for amazing minecraft avatar service
+Cravatar for amazing 3d minecraft avatar service
+Maxmertkit for part of the css framework
+*/
+?>
